2efc97b8e32cd8df555cebd89b544cabea293d50
oskarisuonpaa <80285619+oskarisuonpaa@users.noreply.github.com>
2022-09-10 15:05:53 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function loadImage(event) {
+  var reader = new FileReader();
+  reader.onload = function () {
+    var output = document.getElementById("output");
+    output.src = reader.result;
+  };
+  reader.readAsDataURL(event.target.files[0]);
+}
